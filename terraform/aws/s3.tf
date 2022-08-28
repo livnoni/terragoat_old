33ce20a03e2c8d6cde3a97bf8a81061daeee02cf
Livnoni <yehuda.livnoni@gmail.com>
2022-08-28 15:04:23 +0300

    Update terraform/aws/s3.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -9,7 +9,20 @@ resource "aws_s3_bucket" "data" {
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
-    
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
+  bucket = aws_s3_bucket.data.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+
     
   }
   
